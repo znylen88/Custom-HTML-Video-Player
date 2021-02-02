9423097f4b039b7e5d1b5cec59360fa3ab623caa
znylen88 <znylen@gmail.com>
2021-02-02 15:02:59 -0800

    Add play/pause button functionality

--- scripts.js
+++ scripts.js
@@ -18,4 +18,14 @@ function togglePlay() {
     }
 }
 
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
 // Hook up the event listeners
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
