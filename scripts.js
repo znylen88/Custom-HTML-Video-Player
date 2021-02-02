d67358cddb9aea82d6fad51c3bd8e0f625f178dc
znylen88 <znylen@gmail.com>
2021-02-02 15:15:53 -0800

    Add skip functionality

--- scripts.js
+++ scripts.js
@@ -23,9 +23,22 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    console.log(this.value);
+}
+
 // Hook up the event listeners
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
