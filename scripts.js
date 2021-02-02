19fbc65702117b0e8f584755310274c9df279ebb
znylen88 <znylen@gmail.com>
2021-02-02 15:39:33 -0800

    Add toggle progress bar functionality

--- scripts.js
+++ scripts.js
@@ -24,7 +24,6 @@ function updateButton() {
 }
 
 function skip() {
-    console.log(this.dataset.skip);
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
@@ -32,14 +31,29 @@ function handleRangeUpdate() {
     video[this.name] = this.value;
 }
 
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
 // Hook up the event listeners
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', scrub);
