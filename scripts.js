0d7d71ec8059494024d6e410193ba434bd68ee40
znylen88 <znylen@gmail.com>
2021-02-02 14:45:13 -0800

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// Get Our Elements
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build out functions
+
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// Hook up the event listeners
